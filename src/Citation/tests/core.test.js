39f438d8966c0b2eebfa7abea6031abb19ff6edc
rayor <ruben.aranda.7e5@itb.cat>
2023-01-19 11:34:34 +0100

    Fixing tests
    Co-authored-by: walid243 <walid243@users.noreply.github.com>

--- src/Citation/tests/core.test.js
+++ src/Citation/tests/core.test.js
@@ -1,6 +1,16 @@
 
-test(
-  'shoudl be true', () => {
-    expect(true).toBe(true)
-  }
-)
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Citation } from '../style'
+
+const apiData = {
+  text: 'Never before had I felt trapped, so seduced and caught up in a story, Clara explained ,the way I did with that book. Until then, reading was just a duty, a sort of fine one had to pay teachers and tutors without quite knowing why. I had never known the pleasure of reading, of exploring the recesses of the soul, of letting myself be carried away by imagination, beauty, and the mystery of fiction and language. For me all those things were born with that novel. This is a world of shadows, Daniel, and magic is a rare asset. That book taught me that by reading, I could live more intensely. It could give me back the sight I had lost. For that reason alone, a book that didnt matter to anyone, changed my life.',
+  author_img: 'https://images.gr-assets.com/authors/1444199853p2/815.jpg',
+  id: "b'62d280b094c7f129f9c9b1d703e9f780'",
+  book: 'The Shadow of the Wind',
+  author: 'Carlos Ruiz Zafón'
+}
+
+test('The component Citation should render with mocked api data', () => {
+
+})
